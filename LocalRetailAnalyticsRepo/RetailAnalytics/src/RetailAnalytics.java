6f09a1a6d487f466477f2c18f8e201ea0e941a28
JoeGoldberg <joe_gldberg@bmc.com>
2016-08-24 18:41:21 +0000

    Add Dockerfile for building applicaiton and Control-m agent in a single image

--- LocalRetailAnalyticsRepo/RetailAnalytics/src/RetailAnalytics.java
+++ LocalRetailAnalyticsRepo/RetailAnalytics/src/RetailAnalytics.java
@@ -5,6 +5,7 @@ public class RetailAnalytics {
 			// TODO Auto-generated method stub
 			System.out.println("Hello Retail Analytics at BMC Engage 2016!");
 			System.out.println("Test 003");
+			
 		}
 
 	}
