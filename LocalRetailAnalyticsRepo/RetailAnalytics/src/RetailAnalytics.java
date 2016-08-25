64542fc82f231aa3e36dd543e59d072037e92af0
JoeGoldberg <joe_gldberg@bmc.com>
2016-08-25 09:56:47 +0000

    Update test counter

--- LocalRetailAnalyticsRepo/RetailAnalytics/src/RetailAnalytics.java
+++ LocalRetailAnalyticsRepo/RetailAnalytics/src/RetailAnalytics.java
@@ -4,7 +4,7 @@ public class RetailAnalytics {
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 			System.out.println("Hello Retail Analytics at BMC Engage 2016!");
-			System.out.println("Test 006");
+			System.out.println("Test 007");
 			
 		}
 
