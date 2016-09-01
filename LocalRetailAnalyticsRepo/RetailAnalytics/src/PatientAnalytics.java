ae48c3d0a90c765d9a97f43d592f0338f2eb7f75
JoeGoldberg <joe_gldberg@bmc.com>
2016-09-01 20:29:33 +0000

    Engage demo

--- LocalRetailAnalyticsRepo/RetailAnalytics/src/PatientAnalytics.java
+++ LocalRetailAnalyticsRepo/RetailAnalytics/src/PatientAnalytics.java
@@ -4,8 +4,9 @@ public class PatientAnalytics {
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 			System.out.println("Hello Patient Analytics at BMC Engage 2016!");
-			System.out.println("Test 017");
-			System.out.println("Hello Robin and Jason");
+			System.out.println("Test 018");
+			System.out.println("Hello to Robin and Jason");
+
 			
 	
 			
