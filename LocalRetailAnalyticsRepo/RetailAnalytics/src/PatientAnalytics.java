4464402ae869a61d64b0109a5b8abf6006edbd35
JoeGoldberg <joe_gldberg@bmc.com>
2016-09-04 03:02:25 +0000

    Made a change

--- LocalRetailAnalyticsRepo/RetailAnalytics/src/PatientAnalytics.java
+++ LocalRetailAnalyticsRepo/RetailAnalytics/src/PatientAnalytics.java
@@ -4,7 +4,8 @@ public class PatientAnalytics {
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 			System.out.println("Hello Patient Analytics at BMC Engage 2016!");
-			System.out.println("Test 025");
+			System.out.println("Test 026");
+		
 
 
 			
