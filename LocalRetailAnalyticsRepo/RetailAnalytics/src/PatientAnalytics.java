9f85f3fc554689cf1618459ef4a59cd51f864ab4
JoeGoldberg <joe_gldberg@bmc.com>
2016-09-08 14:57:02 +0000

    Reset message

--- LocalRetailAnalyticsRepo/RetailAnalytics/src/PatientAnalytics.java
+++ LocalRetailAnalyticsRepo/RetailAnalytics/src/PatientAnalytics.java
@@ -3,8 +3,8 @@ public class PatientAnalytics {
 	
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
-			System.out.println("Hello Patient Analytics at BMC Engage 2016!");
-			System.out.println("Test 030 Hello Robin and Jason");
+			System.out.println("Patient Analytics at BMC Engage 2016!");
+			System.out.println("Test 030");
 			
 			
 		}
