9255809e93c3f96ad03d1398af51fc64900168d8
JoeGoldberg <joe_gldberg@bmc.com>
2016-11-02 14:02:52 +0000

    Updates for DOES

--- LocalRetailAnalyticsRepo/RetailAnalytics/src/PatientAnalytics.java
+++ LocalRetailAnalyticsRepo/RetailAnalytics/src/PatientAnalytics.java
@@ -4,7 +4,7 @@ public class PatientAnalytics {
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 			System.out.println("Patient Analytics by Control-M!");
-			System.out.println("Test 040 Hello Strata New York");
+			System.out.println("Test 040 Hello DOES San Francisco 2016");
 			
 			
 		}
