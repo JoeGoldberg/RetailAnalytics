034fe98673b62968dceb43e1524940e8218c9812
[Joe Goldberg] <[joe_goldberg@bmc.com]>
2017-06-19 13:49:07 -0400

    Updates for Gartner

--- LocalRetailAnalyticsRepo/RetailAnalytics/src/PatientAnalytics.java
+++ LocalRetailAnalyticsRepo/RetailAnalytics/src/PatientAnalytics.java
@@ -3,9 +3,10 @@ public class PatientAnalytics {
 	
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
+			System.out.println("==================================================");			
 			System.out.println("Patient Analytics by Control-M!");
 			System.out.println("Test 050 Hello Gartner and BMC Teams June 21, 2017");
-			
+			System.out.println("==================================================");
 			
 		}
 
