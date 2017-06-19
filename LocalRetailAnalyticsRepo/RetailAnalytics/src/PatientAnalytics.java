263395cfe407f1220b2ef7ff2c8b7f6f204b9c29
[Joe Goldberg] <[joe_goldberg@bmc.com]>
2017-06-19 08:30:21 -0400

    Updated Welcome Message for Gartner SAS

--- LocalRetailAnalyticsRepo/RetailAnalytics/src/PatientAnalytics.java
+++ LocalRetailAnalyticsRepo/RetailAnalytics/src/PatientAnalytics.java
@@ -4,7 +4,7 @@ public class PatientAnalytics {
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 			System.out.println("Patient Analytics by Control-M!");
-			System.out.println("Test 040 Hello DOES San Francisco 2016");
+			System.out.println("Test 050 Hello Gartner and BMC Teams June 21, 2017");
 			
 			
 		}
